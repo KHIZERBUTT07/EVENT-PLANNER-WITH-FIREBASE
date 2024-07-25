97e6d3f2cd15eb21bdd20c896079b9bbe0941517
KHIZERBUTT07 <153557362+KHIZERBUTT07@users.noreply.github.com>
2024-07-25 21:11:17 +0500

    up

--- auth/signup/app.js
+++ auth/signup/app.js
@@ -1,7 +1,9 @@
 import{
     auth ,
+    onAuthStateChanged ,
     createUserWithEmailAndPassword ,
     doc ,
+    db ,
     setDoc ,
     storage ,
     ref ,
@@ -17,10 +19,13 @@ import{
 
 
 const signup_btn = document.getElementById("signup_form");
+const submit_btn = document.getElementById("submit_btn");
+
+
 signup_btn.addEventListener("submit", function (e) {
     e.preventDefault();
     console.log(e)
-    console.log("form=>",e.target)
+    console.log("form=>",e.target) 
 
     const  img = e.target[0].files[0]; 
     const  email = e.target[1].value; 
@@ -44,27 +49,53 @@ signup_btn.addEventListener("submit", function (e) {
     };
 
     // create account
+
+    submit_btn.disabled = true;
+    submit_btn.innerText = 'Loading...';
     createUserWithEmailAndPassword(auth,email,password)
     .then((user) => {
         console.log("user=>", user.user.uid)
         // upload user Img
         const userRef = ref(storage , `user/${user.user.uid}`);
-        uploadBytes(userRef , img)
+        uploadBytes(userRef ,img)
         .then(() =>{
             console.log("User Image Uploaded")  ;
             // getting url of the image we just uploaded.
             getDownloadURL(userRef)
-            .then((url) =>{
-                console.log("url agya hai=>", url)
+            .then((url) =>{  
+                console.log("url agya hai=>", url);
+
+                // update user info object
+
+                userInfo.img = url
+                // created user document reference
+
+                var userDbref = doc(db , 'users' , user.user.uid);
+
+                // set this document to db
+                setDoc(userRef , userInfo).then(() => {
+                    console.log("User object updated into db")
+                    window,location.href = "/";
+                    submit_btn.disabled = false;
+                    submit_btn.innerText = 'Submit';
+                });
             })
             .catch((err) => console.log("url firbase ni derha"));
+            submit_btn.disabled = false;
+            submit_btn.innerText = 'Submit';
             
 
             
         }).catch(() => {
-            console.log("error in uploading user image")
+            console.log("error in uploading user image");
+            submit_btn.disabled = false;
+            submit_btn.innerText = 'Submit';
         });
-    })
-    .catch((err) => alert(err))
+    }) 
+    .catch((err) => {
+        alert(err),(  submit_btn.disabled = false);
+            submit_btn.innerText = 'Submit';
+            });
+    
     console.log(userInfo)
 });
